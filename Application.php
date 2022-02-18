fbd55a49868d16a3b2f6834180104ef7ab0b7398
asquaredigital <asquaredigitalsolutions@gmail.com>
2022-02-19 02:27:09 +0530

    commit 19-02

--- Application.php
+++ Application.php
@@ -7,22 +7,22 @@
     <meta name="keywords" content="Hotel,management,course,hotel course,hotel management">
     <meta name="description" content="jennys hotel management course">
     <meta name="author" content="fulgent">
-    <script>
+   <!-- Facebook Pixel Code -->
+<script>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
 n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window,document,'script',
+s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
- fbq('init', '643265346139449'); 
+fbq('init', '734829507312363');
 fbq('track', 'PageView');
 </script>
-<noscript>
- <img height="1" width="1" src="https://www.facebook.com/tr?id=643265346139449&amp;ev=PageView
-&amp;noscript=1"/>
-</noscript>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=734829507312363&ev=PageView&noscript=1"
+/></noscript>
 <!-- End Facebook Pixel Code -->
 	<!--font-awesome-->
 	<script defer src="releases/v5.0.7/js/all.js"></script>
